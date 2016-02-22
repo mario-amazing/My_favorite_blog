d5ffb3825cbc68f10047347ab2e03fcb2e0e819d
megosparta <megosparta@mail.ru>
2016-02-22 04:20:32 +0300

    Added contacts form

---
+++ app/models/contact.rb
@@ -0,0 +1,13 @@
+class Contact < MailForm::Base
+  attributes :name,       :validate => true
+  attributes :email,      :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attributes :message,    :validate => true
+  attributes :nickname,   :captcha => true
+  def headers
+    {
+      :to => "megosparta@gmail.com",
+      :subject => "Contact Form",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
